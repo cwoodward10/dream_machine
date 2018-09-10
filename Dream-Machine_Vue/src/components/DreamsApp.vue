00fe928ba01f6c9c581b1a7671a50994d7371e30
cwoodward10 <c.woodward10@gmail.com>
2018-09-09 20:32:53 -0500

    Add Hue support

--- Dream-Machine_Vue/src/components/DreamsApp.vue
+++ Dream-Machine_Vue/src/components/DreamsApp.vue
@@ -18,6 +18,7 @@
 
       <nav class='main-nav'>
         <h1 class='main-title center test-container'>DO YOU DREAM OF DIGITAL PICNICS?</h1>
+        <button v-on:click="toggleColor()"> Button</button>
       </nav>
 
       <article id='create-dream' class='test-container'>
@@ -25,7 +26,7 @@
           <rect class="vert" height="8" width="2" y="1" x="4" />
           <rect height="2" width="8" y="4" x="1" />
         </svg>
-        <button class='create-button one center'>Create Your Own Dream</button>
+        <button v-on:click="toggleColor()" class='create-button one center'>Create Your Own Dream</button>
       </article>
 
     </section>
@@ -68,38 +69,67 @@ export default {
     }
   },
   methods: {
-    checkHueUsername() {
-      if (!this.hueUser) {
-        this.initHue();
-      };
-    },
-    initHue() {
+    initBridge() {
+      console.log('initBridge')
       var hue = jsHue();
       hue.discover().then(bridges => {
-          if(bridges.length === 0) {
-              console.log('No bridges found. :(');
-          }
-          else {
-              bridges.forEach(b => console.log(this.hueBridges.push(b.internalipaddress)));
+        if(bridges.length === 0) {
+            console.log('No bridges found. :(');
+        }
+        else {
+            bridges.forEach(b => this.hueBridges.push(b.internalipaddress));
 
-              this.activeBridge = hue.bridge(this.hueBridges[0]);
+            console.log(this.hueBridges);
 
-              // create user account (requires link button to be pressed)
-              this.activeBridge.createUser('myApp#testdevice').then(data => {
-                  // extract bridge-generated username from returned data
-                  var username = data[0].success.username;
+            this.activeBridge = hue.bridge(this.hueBridges[0]);
 
-                  console.log('New username:', username);
-
-                  // instantiate user object with username
-                  this.hueUser = this.activeBridge.user(username);
-              });
-          }
+            this.checkHueUsername();
+        };
       }).catch(e => console.log('Error finding bridges', e));
+    },
+    checkHueUsername() {
+      console.log('checkHueUsername')
+      if (!localStorage.hueUsername) {
+        this.initUser();
+        console.log('run initUser')
+      } else {
+        console.log(localStorage.hueUsername)
+        const parsedUsername = localStorage.hueUsername;
+        this.hueUser = this.activeBridge.user(parsedUsername);
+        this.hueUser.setLightState(2, { on: false }).then(data => {
+          // process response data, do other things
+          console.log(data);
+        });
+      }
+    },
+    initUser() {
+      // create user account (requires link button to be pressed)
+      this.activeBridge.createUser('dream_machine').then(data => {
+          // extract bridge-generated username from returned data
+          var username = data[0].success.username;
+
+          console.log('New username:', username);
+
+          localStorage.hueUsername = username;
+
+          // instantiate user object with username
+          this.hueUser = this.activeBridge.user(username);
+
+          this.hueUser.setLightState(2, { on: true }).then(data => {
+            // process response data, do other things
+            console.log(data);
+          });
+      })
+    },
+    toggleColor() {
+      this.hueUser.setLightState(2, { on: true, bri: 128, hue: Math.round(Math.random() * 65000) }).then(data => {
+        // process response data, do other things
+        console.log(data);
+      });
     },
   },
   created() {
-    this.checkHueUsername();
+    this.initBridge()
   },
 }
 
